bd54158ba3165df03c45dcfbc88c09af0a264c73
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-02-20 23:32:36 +0530

    added signup api

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,23 +1,38 @@
-const express = require('express'); 
+const express = require("express");
 const app = express();
-var cors = require('cors')
+var cors = require("cors");
+const bcrypt = require("bcrypt");
 
-const {connectToDatabase} = require("./config/database")
-const User = require("./models/userModel"); 
+const { connectToDatabase } = require("./config/database");
+const User = require("./models/userModel");
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.post("/signup", async(req, res) => {
+  const { firstName, lastName, emailId, password } = req.body;
+  try {
+    const securePassword = await bcrypt.hash(password,10); 
+    const user = new User({
+        firstName,
+        lastName,
+        emailId,
+        password:securePassword
+    });
+    await user.save()
+    res.status(200).send("Account created")
+  } catch (error) {
+    res.status(500).send("Error" +" " + error.message);
+  }
+});
 
-app.post("/signup" , (req,res)=>{
-   
-})
-
-connectToDatabase().then(()=>{
-    console.log("connection Made")
-    app.listen(3000,()=>{
-        console.log("server started")
-    })
-}).catch((err)=>{
-    console.log(err)
-})
+connectToDatabase()
+  .then(() => {
+    console.log("connection Made");
+    app.listen(3000, () => {
+      console.log("server started");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -1,20 +1,42 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      minLength: 3,
+      maxLength: 20,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      validate: function (value) {
+        return validator.isEmail(value);
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: function (value) {
+        return validator.isStrongPassword(value);
+      },
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
-const User = mongoose.model("User", userSchema); 
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
