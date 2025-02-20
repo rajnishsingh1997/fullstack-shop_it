11b18f31817c71c73a3f70ada7ad665d4135a5bb
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-02-20 22:59:19 +0530

    created user model

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const {connectToDatabase} = require("./config/database")
+const User = require("./models/userModel"); 
 
 connectToDatabase().then(()=>{
     console.log("connection Made")
