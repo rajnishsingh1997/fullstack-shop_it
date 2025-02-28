9d6e2189b0220a33d07a42f199fe957ad444c18a
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-02-28 12:01:22 +0530

    fixed product api and frontend change

--- Backend/src/routes/product.js
+++ Backend/src/routes/product.js
@@ -1,53 +1,60 @@
-const express = require("express"); 
+const express = require("express");
 const productAuth = express.Router();
 const Product = require("../models/productModel");
-const{userCheckMiddleware} = require("../Middleware/authMiddle");
+const { userCheckMiddleware } = require("../Middleware/authMiddle");
 
-productAuth.get("/products",userCheckMiddleware, async (req, res) => {
-    try {
+productAuth.get("/products", userCheckMiddleware, async (req, res) => {
+  try {
+    const { categories } = req.query;
+    if (categories) {
+      const specificProduct = await Product.find({ category: categories });
+      res.status(200).send(specificProduct);
+    } else {
       const products = await Product.find({});
-      if (!products) {
+      if (products.length===0) {
         res.status(400).send("Something went wrong");
       }
       res.status(200).send(products);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: "Something went wrong, please try again later" });
     }
-  });
-  
-  productAuth.get("/categories",userCheckMiddleware, async (req, res) => {
-    try {
-      res
-        .status(200)
-        .send(["electronics", "jewelery", "men's clothing", "women's clothing"]);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: "Something went wrong, please try again later" });
-    }
-  });
-  
-  productAuth.get("/products/category/:id",userCheckMiddleware, async (req, res) => {
-    const category = req?.params;
-    const { id } = category;
-    if (!category) {
-      return res.status(500).send("something went wrong!");
-    }
-    try {
-      const product = await Product.find({ category: id });
-      if (product.length === 0) {
-        res.status(400).send("unable to find any product");
-      }
-      res.status(200).send(product);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: "Something went wrong, please try again later" });
-    }
-  });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Something went wrong, please try again later" });
+  }
+});
 
+productAuth.get("/categories", userCheckMiddleware, async (req, res) => {
+  try {
+    res
+      .status(200)
+      .send(["electronics", "jewelery", "men's clothing", "women's clothing"]);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Something went wrong, please try again later" });
+  }
+});
+
+// productAuth.get("/product/categories", async (req, res) => {
+//   try {
+//     const { categories } = req.query;
+//     if (!categories) {
+//       throw new Error("Something went wrong!");
+//     }
+//     const product = await Product.find({ category: categories });
+//     if (product.length === 0) {
+//       return res
+//         .status(404)
+//         .json({ error: "No products found for this category" });
+//     }
+//     res.status(200).send(product);
+//   } catch (error) {
+//     console.log(error);
+//     res
+//       .status(500)
+//       .json({ error: "Something went wrong, please try again later" });
+//   }
+// });
 
 module.exports = productAuth;
 
